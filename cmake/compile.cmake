c2b4b7d7d07694d7c656cb639d236c1444569dba
Ivan Piskarev <piskarev.iv@gmail.com>
2026-05-31 02:59:50 +0300

    fix pipeline

--- cmake/compile.cmake
+++ cmake/compile.cmake
@@ -1,12 +1,11 @@
 
 set(CMAKE_CXX_STANDARD 23)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 message("C++ version: ${CMAKE_CXX_STANDARD}")
 
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2 -Wall -Wextra -Werror")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2 -Wall -Wextra -Wpedantic -Werror")
 
 set(CMAKE_CXX_FLAGS_ASAN "-O0 -g -fsanitize=address,undefined,bounds -fno-sanitize-recover=all"
     CACHE STRING "Compiler flags in asan build"
--- script/setup.sh
+++ script/setup.sh
@@ -1,3 +1,5 @@
+# !!! do not run this on your personal computer !!!
+
 sudo apt-get update
 sudo apt-get install -y cmake build-essential
 sudo apt-get install -y clang-tidy clang-format
@@ -7,3 +9,6 @@ chmod +x /tmp/llvm.sh
 sudo /tmp/llvm.sh 20 all
 
 clang-20 --version
+
+# echo 'CC="clang-20"'    | sudo tee -a /etc/environment
+# echo 'CXX="clang++-20"' | sudo tee -a /etc/environment
