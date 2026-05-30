16a06123b8c2339ea47ea0f36a32f4c44c476a2d
Ivan Piskarev <piskarev.iv@gmail.com>
2026-05-31 02:45:59 +0300

    fix pipeline

--- cmake/compile.cmake
+++ cmake/compile.cmake
@@ -6,10 +6,10 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 message("C++ version: ${CMAKE_CXX_STANDARD}")
 
 
-set(CMAKE_CXX_FLAGS "-O2 -Wall -Wextra -Werror")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2 -Wall -Wextra -Werror")
 
 set(CMAKE_CXX_FLAGS_ASAN "-O0 -g -fsanitize=address,undefined,bounds -fno-sanitize-recover=all"
     CACHE STRING "Compiler flags in asan build"
     FORCE)
 
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+
