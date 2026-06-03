82f37ee60f2ee91d1bd0c0a98756718fd575d359
Ivan Piskarev <piskarev.iv@gmail.com>
2026-06-03 20:48:52 +0300

    fix docker

--- script/setup.sh
+++ script/setup.sh
@@ -3,12 +3,12 @@
 
 # !!! do not run this on your personal computer !!!
 
-apt-get update
-apt-get install -y cmake build-essential
-apt-get install -y clang-tidy clang-format
-apt install libabsl-dev libgtest-dev libbenchmark-dev
-apt install libre2-dev
-apt install pkg-config
+apt update
+apt install -y cmake build-essential
+apt install -y clang-tidy clang-format
+apt install -y libabsl-dev libgtest-dev libbenchmark-dev
+apt install -y libre2-dev
+apt install -y pkg-config
 
 
 wget -qO /tmp/llvm.sh https://apt.llvm.org/llvm.sh
