ca24e0d3f1163da297f345137ec1dd50a38790eb
Ivan Piskarev <piskarev.iv@gmail.com>
2026-06-08 21:03:37 +0300

    disable msan

--- script/build_and_test.sh
+++ script/build_and_test.sh
@@ -2,7 +2,7 @@
 
 (mkdir -p build_asan && cd build_asan && cmake -DCMAKE_BUILD_TYPE=ASAN     .. && make -j7) && bash script/test.sh build_asan &&
 (mkdir -p build_tsan && cd build_tsan && cmake -DCMAKE_BUILD_TYPE=TSAN     .. && make -j7) && bash script/test.sh build_tsan &&
-(mkdir -p build_msan && cd build_msan && cmake -DCMAKE_BUILD_TYPE=MSAN     .. && make -j7) && bash script/test.sh build_msan &&
+# (mkdir -p build_msan && cd build_msan && cmake -DCMAKE_BUILD_TYPE=MSAN     .. && make -j7) && bash script/test.sh build_msan &&
 (mkdir -p build      && cd build      && cmake -DCMAKE_BUILD_TYPE=Release  .. && make -j7) && bash script/test.sh build      &&
 (mkdir -p build_cov  && cd build_cov  && cmake -DCMAKE_BUILD_TYPE=COVERAGE .. && make -j7) && bash script/test.sh build_cov  &&
 echo ALL TESTS PASSED
